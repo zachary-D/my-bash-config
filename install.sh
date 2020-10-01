07c1459b8020bd46f00301004536d4bef4d1f266
Zachary Damato <zacharyd1@live.com>
2020-09-30 21:06:34 -0400

    feat: the installer will now add cronjobs to update the config file when the computer starts and every day at noon

--- install.sh
+++ install.sh
@@ -6,3 +6,21 @@ if [[ "$( grep -o $config_path ~/.bashrc )" == "" ]]; then
 else
 	echo "Config file already sourced"
 fi
+
+# $1 - string to check for; $2 - command to add
+function addCommandtoCronIfMissing() {
+	commandToAdd="$2"
+	if [[ "$2" == "" ]]; then
+		commandToAdd="$1"
+	fi
+
+	if [[ $( crontab -l | grep -F -o "$1") == "" ]]; then
+		( crontab -l; echo "$commandToAdd" ) | crontab -
+	else
+		echo "$1 already found"
+	fi
+}
+
+cronUpdateCommand="cd $(pwd); git pull >> /dev/null"
+addCommandtoCronIfMissing "0 2 * * * $cronUpdateCommand"
+addCommandtoCronIfMissing "@reboot $cronUpdateCommand"
