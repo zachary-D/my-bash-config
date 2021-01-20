ab82829c4e00023a39ceaa305c73f57e20d6685c
Zachary Damato <zacharyd1@live.com>
2021-01-20 01:13:43 -0500

    fix: corrected an issue where the background updater would not print the update successful message even if it was successful

--- backgroundUpdate.sh
+++ backgroundUpdate.sh
@@ -7,7 +7,7 @@ TIME_TO_WAIT_BETWEEN_UPDATES=$((24 * 60 * 60)) # one day
 scriptDir="$( cd "$(dirname "${BASH_SOURCE}")" >/dev/null 2>&1 ; pwd -P )/"
 
 if [[ ! -f "$scriptDir""lastUpdatedAt" ]] || [[ "$(cat $scriptDir/lastUpdatedAt)" < $(( $(date +%s) - $TIME_TO_WAIT_BETWEEN_UPDATES )) ]]; then
-	if [[ $($scriptDir/updater.sh --error-on-up-to-date) ]]; then
+	if $scriptDir/updater.sh --error-on-up-to-date; then
 		echo "Bash configuration auto-updated in background; restart terminal to load changes"
 	fi
 fi
