d5c93c3129f7fa00b49881255e5e3b1bce1247c8
Zachary Damato <zacharyd1@live.com>
2021-07-04 16:05:49 -0400

    feat: allowed the redirect-db-to-IP aliases to be enabled/disabled independently of setting WSL_HOST

--- bash-config.sh
+++ bash-config.sh
@@ -13,12 +13,18 @@ alias gstat="git status"
 alias gdiff="git diff --ignore-space-change"
 alias gdiffc="gdiff --cached"
 
-# redirect mariadb to localost over the network so it can connect to the server running on the windows-side (WSL)
-# set WSL_HOST in the machine-specific bash config before this file is loaded to enable
-if [[ "$WSL_HOST" != "" ]]; then
-	alias mariadb="mariadb -h $WSL_HOST"
-	alias mysqldump="mysqldump -h $WSL_HOST"
-fi
+# redirect mariadb to the WSL host over the network so it can connect to the server running on the windows-side (WSL)
+# set REDIRECT_DB in the machine-specific bash config before this file is loaded to enable
+case "$REDIRECT_DB" in
+	WSL_HOST)
+		alias mariadb="mariadb -h $WSL_HOST"
+		alias mysqldump="mysqldump -h $WSL_HOST"
+	;;
+	LOCALHOST_IP)
+		alias mariadb="mariadb -h 127.0.0.1"
+		alias mysqldump="mysqldump -h 127.0.0.1"
+	;;
+esac
 
 # functional aliases
 alias mysql=mariadb
