c8e4fb483daf5b1387392118db3c81fb9ad67b83
Zachary Damato <zacharyd1@live.com>
2023-05-08 12:25:45 -0400

    fix: prevented the updater script from creating merges when trying to pull diverged branches

--- updater.sh
+++ updater.sh
@@ -2,7 +2,7 @@
 scriptDir="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/"
 cd "$scriptDir"
 date +%s > "$scriptDir""lastUpdatedAt"
-if [[ "$(git pull)" != "Already up to date." ]]; then
+if [[ "$(git pull --ff-only)" != "Already up to date." ]]; then
 	./install.sh
 elif [[ "$1" == "--error-on-up-to-date" ]]; then
 	exit 1
